9069c65f5f8e6f292030e6cb2b906a850708fce8
Alan Soares <alan.ssoares@live.com>
2016-07-15 10:18:15 -0300

    Adicionando service para busca de posts

--- js/components/posts/posts.controller.js
+++ js/components/posts/posts.controller.js
@@ -5,45 +5,23 @@
 		.module('posts')
 		.controller('PostsCtrl', PostsCtrl);
 	
-	function PostsCtrl() {
-	    var vmPostsCtrl = this;
-	    
-	    vmPostsCtrl.posts = [{
-	    title: 'Como criar um Blog', 
-	    author: 'Alan Soares', 
-	    date: '13/07/2016', 
-	    description: 'Este é um post inicial que tem como objetivo visualizar uma lista de posts. Cada post possui um conjunto de informações que identificam o post'
-	    },
-	    {
-	    title: 'Como criar um Blog', 
-	    author: 'Alan Soares', 
-	    date: '13/07/2016', 
-	    description: 'Este é um post inicial que tem como objetivo visualizar uma lista de posts. Cada post possui um conjunto de informações que identificam o post'
-	    },
-	    {
-	    title: 'Como criar um Blog', 
-	    author: 'Alan Soares', 
-	    date: '13/07/2016', 
-	    description: 'Este é um post inicial que tem como objetivo visualizar uma lista de posts. Cada post possui um conjunto de informações que identificam o post'
-	    },
-	    {
-	    title: 'Como criar um Blog', 
-	    author: 'Alan Soares', 
-	    date: '13/07/2016', 
-	    description: 'Este é um post inicial que tem como objetivo visualizar uma lista de posts. Cada post possui um conjunto de informações que identificam o post'
-	    },
-	    {
-	    title: 'Como criar um Blog', 
-	    author: 'Alan Soares', 
-	    date: '13/07/2016', 
-	    description: 'Este é um post inicial que tem como objetivo visualizar uma lista de posts. Cada post possui um conjunto de informações que identificam o post'
-	    },
-	    {
-	    title: 'Como criar um Blog', 
-	    author: 'Alan Soares', 
-	    date: '13/07/2016', 
-	    description: 'Este é um post inicial que tem como objetivo visualizar uma lista de posts. Cada post possui um conjunto de informações que identificam o post'
-	    }];
+	var baseUrl = 'https://github.com/alanssoares/alanssoares.github.io/tree/master/js/components/posts/views/';
+
+	PostsCtrl.$inject = ['PostService'];
+	
+	function PostsCtrl(PostService) {
+	    	var vmPostsCtrl = this;
+		vmPostsCtrl.showList = true;
+		vmPostsCtrl.showOnlyOne = false;
+		vmPostsCtrl.showPost = showPost;
+		vmPostsCtrl.posts = PostService.getPosts();
+		vmPostsCtrl.post = {};
+		
+	    	function showPost(post){
+			vmPostsCtrl.showList = false;
+			vmPostsCtrl.showOnlyOne = true;
+			vmPostsCtrl.post = post;
+	    }
 	}
 	
 })(angular);
