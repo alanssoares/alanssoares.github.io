8f2f293d797be2d309679eb8757cfcb1b00016d5
Alan Soares <alan.ssoares@live.com>
2016-07-15 10:35:38 -0300

    alterando url

--- js/components/posts/posts.service.js
+++ js/components/posts/posts.service.js
@@ -5,7 +5,7 @@
 		.module('posts')
 		.factory('PostService', PostService);
 		
-	var baseUrl = '\'https://alanssoares.github.io/js/components/posts/views/';
+	var baseUrl = 'js/components/posts/views/';
 	
 	function PostService(){
 		return {
@@ -19,7 +19,7 @@
 		    	author: 'Alan Soares', 
 		    	date: '15/07/2016', 
 		    	description: 'Este foi o primeiro post publicado na página. Espero que aproveitem ao máximo todo o conteúdo que estarei compartilhando.',
-		    	url: baseUrl + 'boas-vindas.html\''
+		    	url: baseUrl + 'boas-vindas.html
     		}];
     		
 			return posts;
