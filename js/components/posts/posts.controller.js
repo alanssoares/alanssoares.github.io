ef30440b6f5f647fd8d5fb26fdbcd16c9b9caaa8
Alan Santos <alan.ssoares@live.com>
2016-07-15 09:40:40 -0300

    Adicionando mock de posts

--- js/components/posts/posts.controller.js
+++ js/components/posts/posts.controller.js
@@ -12,7 +12,37 @@
 	    title: 'Como criar um Blog', 
 	    author: 'Alan Soares', 
 	    date: '13/07/2016', 
-	    content: 'Este é um post inicial que tem como objetivo visualizar uma lista de posts. Cada post possui um conjunto de informações que identificam o post'
+	    description: 'Este é um post inicial que tem como objetivo visualizar uma lista de posts. Cada post possui um conjunto de informações que identificam o post'
+	    },
+	    {
+	    title: 'Como criar um Blog', 
+	    author: 'Alan Soares', 
+	    date: '13/07/2016', 
+	    description: 'Este é um post inicial que tem como objetivo visualizar uma lista de posts. Cada post possui um conjunto de informações que identificam o post'
+	    },
+	    {
+	    title: 'Como criar um Blog', 
+	    author: 'Alan Soares', 
+	    date: '13/07/2016', 
+	    description: 'Este é um post inicial que tem como objetivo visualizar uma lista de posts. Cada post possui um conjunto de informações que identificam o post'
+	    },
+	    {
+	    title: 'Como criar um Blog', 
+	    author: 'Alan Soares', 
+	    date: '13/07/2016', 
+	    description: 'Este é um post inicial que tem como objetivo visualizar uma lista de posts. Cada post possui um conjunto de informações que identificam o post'
+	    },
+	    {
+	    title: 'Como criar um Blog', 
+	    author: 'Alan Soares', 
+	    date: '13/07/2016', 
+	    description: 'Este é um post inicial que tem como objetivo visualizar uma lista de posts. Cada post possui um conjunto de informações que identificam o post'
+	    },
+	    {
+	    title: 'Como criar um Blog', 
+	    author: 'Alan Soares', 
+	    date: '13/07/2016', 
+	    description: 'Este é um post inicial que tem como objetivo visualizar uma lista de posts. Cada post possui um conjunto de informações que identificam o post'
 	    }];
 	}
 	
